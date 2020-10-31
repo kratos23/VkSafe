b8bad09853cffb51a9ad7d5a59e3be0f4c6bb910
kaktus23 <nnnnn94@inbox.ru>
2020-10-31 22:52:28 +0300

    Release

--- app/src/main/java/com/pavelkrylov/vsafe/logic/network/OkHttp.kt
+++ app/src/main/java/com/pavelkrylov/vsafe/logic/network/OkHttp.kt
@@ -13,7 +13,7 @@ object OkHttp {
             .build()
     }
 
-    const val BASE_URL = "http://192.168.68.105:8080"
+    const val BASE_URL = "http://185.104.113.150:8080"
     const val MARKETS_URL = "$BASE_URL/markets"
     const val NEW_ORDER_URL = "$BASE_URL/orders/new"
     const val CUSTOMER_ORDERS_URL = "$BASE_URL/customer/orders"
